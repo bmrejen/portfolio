7393546131d2a2b7acfce2111b7093afa6104c74
Benoit Mrejen <benoit.mrejen@gmail.com>
2025-07-15 04:44:47 +0800

    fix imports

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled, { ThemeProvider } from 'styled-components';
-import { Head, Loader, Nav, Social, Email, Footer } from '@components';
-import { GlobalStyle, theme } from '@styles';
+import { Head } from './head';
+import Loader from './loader';
+import Nav from './nav';
+import Social from './social';
+import Email from './email';
+import Footer from './footer';
+import { GlobalStyle, theme } from '../styles';
 
 // https://medium.com/@chrisfitkin/how-to-smooth-scroll-links-in-gatsby-3dc445299558
 if (typeof window !== 'undefined') {
